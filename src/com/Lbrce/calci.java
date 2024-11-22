03362c10fe9944f5de68f49d6bd4db6a8360fbc0
Sai <Sai@DESKTOP-9CG70J6>
2024-11-22 09:39:37 +0530

    commit

---
+++ src/com/Lbrce/calci.java
@@ -0,0 +1,41 @@
+package com.Lbrce;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+class calci {
+    public static void main(String[] args) throws FileNotFoundException {
+        // Load file
+    	
+        File file = new File("C:\\teja\\file.txt");
+        Scanner scanner = new Scanner(file);
+
+        // Read numbers and operation from file
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+        char operation = scanner.next().charAt(0);
+        scanner.close();
+
+        // Perform operation using switch
+        switch (operation) {
+            case '+':
+                System.out.println("Sum is: " + (num1 + num2));
+                break;
+            case '-':
+                System.out.println("Difference is: " + (num1 - num2));
+                break;
+            case '*':
+                System.out.println("Product is: " + (num1 * num2));
+                break;
+            case '/':
+                if (num2 != 0) {
+                    System.out.println("Quotient is: " + (num1 / num2));
+                } else {
+                    System.out.println("Error: Division by zero is not allowed.");
+                }
+                break;
+            default:
+                System.out.println("Invalid operation.");
+        }
+    }
+}
